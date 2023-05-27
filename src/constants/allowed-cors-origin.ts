91b8a4e5fa53a622bf79f0fa79be2c12acce5541
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-05-28 02:27:08 +0530

    update: cors origin

--- src/constants/allowed-cors-origin.ts
+++ src/constants/allowed-cors-origin.ts
@@ -2,4 +2,6 @@ export const origin = [
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:3002",
+    "http://localhost:3002",
+    "https://employee-innvo8.vercel.app",
 ];
