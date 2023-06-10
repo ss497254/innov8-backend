bff4fe738586cd7f7b16002caebc5bf9b41f4e29
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-06-10 22:06:44 +0530

    update: project score to calculate average and variance

--- src/services/project.score.service.ts
+++ src/services/project.score.service.ts
@@ -7,6 +7,8 @@ import {
     getItemById,
     updateItem,
 } from "../firebase";
+import { removeKey } from "../utils/lodash";
+import pickProperty from "../utils/pickProperty";
 import { projectScoreValidation } from "../validations";
 
 const ProjectScoresTable = "projects-scores";
@@ -16,7 +18,13 @@ export const getScoreByProjectId = async (projectId: string) => {
     const data = (
         await getCollectionData(ProjectScoresTable)
             .where("projectId", "==", projectId)
-            .select("interviewTitle", "employee", "coach")
+            .select(
+                "interviewTitle",
+                "employee",
+                "coach",
+                "average",
+                "variance"
+            )
             .get()
     ).docs.map((doc) => ({
         id: doc.id,
@@ -43,12 +51,12 @@ export const getUserScoreById = async (
 ) => {
     const score = await getItemById(ProjectScoresTable, interviewId);
     if (!score.exists) throw new Error("Project score not found");
-    session;
+
     return {
         updatedAt: score.updateTime?.toMillis(),
         score: score
             .data()
-            ?.[session.role]?.filter((x: any) => x.userId === session.id)?.[0],
+            ?.[session.role]?.find((x: any) => x.userId === session.id),
     };
 };
 
@@ -65,16 +73,79 @@ export const saveProjectScore = async (
         completed: FieldValue.arrayUnion(data.userId),
     });
 
-    const score = await getItemById(ProjectScoresTable, id);
-    if (score.exists) {
-        return await updateItem(ProjectScoresTable, id, {
-            [role]: FieldValue.arrayUnion(data),
-        });
+    const empty = {
+        score: data.score.map((x) => ({
+            ...x,
+            questions: Array(x.questions.length).fill(0),
+        })),
+        overallRating: 0,
+    };
+
+    const res = await getItemById(ProjectScoresTable, id);
+    if (res.exists) {
+        const kabil = res.data()!;
+
+        if (role === "employee") {
+            const totalEmployees = kabil.employee.length;
+            const average = {
+                score: data.score.map((x, idx) => ({
+                    ...x,
+                    questions: x.questions.map(
+                        (y, idy) =>
+                            kabil.average.score[idx].questions[idy] +
+                            (y - kabil.average.score[idx].questions[idy]) /
+                                (totalEmployees + 1)
+                    ),
+                })),
+                overallRating:
+                    kabil.average.overallRating +
+                    (data.overallRating - kabil.average.overallRating) /
+                        (totalEmployees + 1),
+            };
+
+            const variance = empty;
+
+            kabil.employee.forEach((e: typeof data) => {
+                variance.overallRating +=
+                    (e.overallRating - average.overallRating) ** 2;
+
+                e.score.forEach((x, idx) =>
+                    x.questions.forEach((y, idy) => {
+                        variance.score[idx].questions[idy] +=
+                            (y - average.score[idx].questions[idy]) ** 2;
+                    })
+                );
+            });
+
+            if (totalEmployees > 1) {
+                variance.overallRating /= totalEmployees;
+
+                variance.score.forEach((x) => ({
+                    ...x,
+                    questions: x.questions.map((y) => y / totalEmployees),
+                }));
+            }
+
+            return await updateItem(ProjectScoresTable, id, {
+                [role]: FieldValue.arrayUnion(data),
+                average,
+                variance,
+            });
+        } else {
+            return await updateItem(ProjectScoresTable, id, {
+                [role]: FieldValue.arrayUnion(data),
+            });
+        }
     }
 
     return await addItemWithId(ProjectScoresTable, id, {
         projectId,
         interviewTitle,
         [role]: FieldValue.arrayUnion(data),
+        average:
+            role === "employee"
+                ? pickProperty(data, ["score", "overallRating"])
+                : empty,
+        variance: empty,
     });
 };
